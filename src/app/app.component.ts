4168f06e3324ce80a7a1bc06319a0af7ea123700
AiyahLappy <gycf84@yahoo.com>
2017-12-27 11:50:04 -0800

    using material datatable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,20 @@
 import { Component } from '@angular/core';
-import {MatTableDataSource, MatSort} from '@angular/material';
+import {MatTableDataSource} from '@angular/material';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular Initiative Tracker';
+  title = 'Angular.io Initiative Tracker';
   id=0;
   name: string;
   init: number;
   hp: number;
   notes: string;
-  hpChange= [];
+  hpChange= [0];
   public list: object[] = [];
+  data = new MatTableDataSource(this.list)
 
   heal(person, index):void{
     person.hp += this.hpChange[index]
@@ -27,17 +28,18 @@ export class AppComponent {
     this.list.push({
       name:this.name, 
       init:this.init, 
-      id:this.id,
+      id:this.list.length+1,
       hp:this.hp,
       notes:this.notes
     });
-    this.id++;
     this.hpChange.push(0)
+    this.data = new MatTableDataSource(this.list)
   }
   next():void{
     let temp = this.list[0]
     this.list.splice(0,1)
-    this.list.push(temp) 
+    this.list.push(temp)
+    this.data = new MatTableDataSource(this.list)
   }
   sort():void {
     if (this.list.length > 1){
@@ -46,15 +48,24 @@ export class AppComponent {
         if(a['init'] < b['init']){return 1}
         return 0
       })
+      for (let i = 0;i < this.list.length;i++){
+        this.list[i]['id'] = i+1
+      }
     }
+    this.data = new MatTableDataSource(this.list)
   }
   remove(data):void{
     let arrLength = this.list.length
+    let theOne;
     for (let i = 0;i < arrLength;i++){
       if(this.list[i]['id'] === data){
-        this.list.splice(i,1)
-        break;
+        theOne=i
       }
     }
+    this.list.splice(theOne,1);
+    for (let i = 0;i < this.list.length;i++){
+      this.list[i]['id'] = i+1
+    }
+    this.data = new MatTableDataSource(this.list)
   }
 }
